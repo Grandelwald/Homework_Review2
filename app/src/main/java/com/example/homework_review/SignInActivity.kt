f74342391df634f01139005e0b133de919242ab1
Grandelwald <artjr97@naver.com>
2021-12-09 21:09:04 +0900

    [FEAT] #1 - SignIn Activity / Home Activity 연결

--- app/src/main/java/com/example/homework_review/SignInActivity.kt
+++ app/src/main/java/com/example/homework_review/SignInActivity.kt
@@ -1,15 +1,31 @@
 package com.example.homework_review
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.homework_review.databinding.ActivitySignInBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySignInBinding.inflate(layoutInflater)
+        val homeIntent = Intent(this,HomeActivity::class.java)
+
+        binding.btnLogin.setOnClickListener {
+            if(binding.edtvNewid.text==null||binding.edtvNewpw.text==null){
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"환영합니다",Toast.LENGTH_SHORT).show()
+                startActivity(homeIntent)
+            }
+        }
+
+
         setContentView(binding.root)
     }
 
